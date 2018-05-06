3c202d77bfc45273e0d7677f7dd9c6d1fe33321b
TomaszZ <fenskolo@gmail.com>
2018-05-06 14:32:58 +0200

    add comboBox with value and newRow

--- EasyInvoice/EasyInvoice/MainWindow.xaml.cs
+++ EasyInvoice/EasyInvoice/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ namespace EasyInvoice
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DataTable xcv;
         public MainWindow()
         {
             InitializeComponent();            
@@ -35,20 +36,61 @@ namespace EasyInvoice
             dt.Columns.Add("Stawka VAT");
             dt.Columns.Add("Kwota VAT");
             dt.Columns.Add("Wartość Brutto");
-            DataRow row = dt.NewRow();
+            //DataRow row = dt.NewRow();
 
-            for (int i = 0; i<dt.Columns.Count; i++)
-            {
-                row[i] = i;
-            }
-            dt.Rows.Add(row);
+            //for (int i = 0; i<dt.Columns.Count; i++)
+            //{
+            //    row[i] = i;
+            //}
+            //dt.Rows.Add(row);
            // dt.Rows.Add();
 
-            xDG.DataSource = dt.DefaultView;
-            
+            xDG.DataContext = dt.DefaultView;
+            xcv = dt;
          //   xDG.DataItems.Add(new ComboBox());
         }
+
+        private void XDG1_Loaded(object sender, RoutedEventArgs e)
+        {
+            PopulateCombo(xDG);
+           // this.xDG.AddNewRowSettings.AllowAddNewRow = Infragistics.Controls.Grids.AddNewRowLocation.b;
+        }
+
+        public void PopulateCombo(XamDataGrid grid)
+        {
+            ComboBoxField cbJ = grid.DefaultFieldLayout.Fields["J.m."] as ComboBoxField;
+            //  DataView dv = grid.DataContext as DataView;
+
+            List<string> nameList = new List<string>
+            {
+                "szt.",
+                "godz.",
+                "m2",
+                "op.",
+                "mc.",
+                "ton",
+                "km.",
+                "m3."
+            };
+            ComboBoxField cbV = grid.DefaultFieldLayout.Fields["Stawka VAT"] as ComboBoxField;
+
+
+            List<string> stawkaList = new List<string>
+            {
+                "23%"
+            };
+            cbJ.ItemsSource = nameList;
+            cbV.ItemsSource = stawkaList;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+          //  xDG.DefaultFieldLayout.ac
+            var x =xcv.Rows.Count;
+        }
     }
+
 
     public enum D
     {
